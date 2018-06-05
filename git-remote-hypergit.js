7865fd3f4aa4d08e2371743d1820ec319cea4c7c
noffle <sww@eight45.net>
2018-06-04 20:33:42 -0700

    chore: add missing dep

--- git-remote-hypergit.js
+++ git-remote-hypergit.js
@@ -3,7 +3,7 @@
 var toPull = require('stream-to-pull-stream')
 var pull = require('pull-stream')
 var hyperdb = require('hyperdb')
-var Repo = require('.')
+var Repo = require('hyperdb-git-repo')
 var gitRemoteHelper = require('pull-git-remote-helper')
 
 var name = process.argv[3].replace('hypergit://', '')
