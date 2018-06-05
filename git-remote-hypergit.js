0bf1dcbcd6fc28e15cbfc51eb062df1ee6668b91
noffle <sww@eight45.net>
2018-06-04 20:30:31 -0700

    init + impl

---
+++ git-remote-hypergit.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+var toPull = require('stream-to-pull-stream')
+var pull = require('pull-stream')
+var hyperdb = require('hyperdb')
+var Repo = require('.')
+var gitRemoteHelper = require('pull-git-remote-helper')
+
+var name = process.argv[3].replace('hypergit://', '')
+
+var db = hyperdb(name)
+
+db.ready(function () {
+  pull(
+    toPull(process.stdin),
+    gitRemoteHelper(Repo(db)),
+    toPull(process.stdout)
+  )
+})
